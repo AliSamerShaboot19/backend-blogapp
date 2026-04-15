556904a17eacd5bfb6409fb239637cb5913c77ac
AliSamerShaboot19 <www.ali.s.samer@gmail.com>
2026-04-15 23:13:32 +0300

    Fix email verification link in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
   await verificationToken.save();
 
-  const link = `${apiUrl}/api/auth/${user._id}/verify/${verificationToken.token}`;
+  const link = `https://blogappcl.netlify.app/api/auth/${user._id}/verify/${verificationToken.token}`;
   const htmlTemplate = `
     <div>
       <p>click on the link below to verify your email</p>
